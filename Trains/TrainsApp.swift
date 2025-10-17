70c37a27d4beb1dc3fe24dfce62d763df7c973e7
Brabus098 <vovan1998888@mail.ru>
2025-10-17 21:47:01 +0300

    Добавлены стандартные файлы.

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Владимир on 17.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
