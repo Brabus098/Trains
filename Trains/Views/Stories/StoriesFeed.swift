9e38419851428a7644af91bb279c053fd03f0f1e
Brabus098 <vovan1998888@mail.ru>
2025-11-20 00:02:59 +0300

    chore(project): обновить структуру и добавить утилиты
    - Изменена организация файлов в проекте
    - Добавлено расширение View для отладки шрифтов (printAvailableFonts)
    - Добавлены новые assets ресурсы
    
    Улучшена структура проекта и добавлены вспомогательные инструменты для разработки.

--- Trains/Views/Stories/StoriesFeed.swift
+++ Trains/Views/Stories/StoriesFeed.swift
@@ -1,14 +1,10 @@
-//
 //  StoriesFeed.swift
-//  Trains
-//
-//  Created by Владимир on 18.11.2025.
-//
+
 import SwiftUI
 
 struct StoriesFeed: View {
     let imageViewArray = ["1","2","3","4","5"]
-
+    
     var body: some View {
         ScrollView(.horizontal) {
             LazyHStack(spacing: 10) {
