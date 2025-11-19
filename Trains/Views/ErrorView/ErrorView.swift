1a0455eb203c10b03c9f7bccb82fc38b04f91845
Brabus098 <vovan1998888@mail.ru>
2025-11-19 23:47:22 +0300

    feat(error): добавить экраны ошибок сервера и сети
    Добавлены специализированные экраны для обработки ошибок:
    - Экран ошибки сервера (500 и подобные)
    - Экран отсутствия интернет-соединения
    
    Экраны содержат понятные сообщения для пользователя и кнопки для повторных попыток.

---
+++ Trains/Views/ErrorView/ErrorView.swift
@@ -0,0 +1,26 @@
+//  ServerErrorView.swift
+
+import SwiftUI
+
+struct ErrorView: View {
+    
+    let imageHeight: CGFloat = 300
+    let imageName: String
+    let text: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: imageHeight, height: imageHeight)
+                .padding()
+            Text(text)
+                .font(.custom("SFPro-Bold", size: 24))
+        }
+    }
+}
+//
+//#Preview {
+//    ErrorView(imageName: "NoInternetError", text: "Нет интернета")
+//}
