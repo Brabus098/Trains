26caf95bd68ef35718e5658fe46d35617df4bbf2
Brabus098 <vovan1998888@mail.ru>
2025-11-22 11:04:29 +0300

    feat(settings): добавить экран настроек
    Добавлен полнофункциональный экран настроек приложения. Реализованы основные настройки и конфигурации для пользователя.

--- Trains/Views/Settings/SettingsScreenView.swift
+++ Trains/Views/Settings/SettingsScreenView.swift
@@ -3,13 +3,68 @@
 import SwiftUI
 
 struct SettingsScreenView: View {
+    @Environment(ThemeManager.self) var themeManager
+    @State var navigationPath: NavigationPath = NavigationPath()
+    @Binding var hideTabBar: Bool
+    
     var body: some View {
-        ZStack {
-            Color.background.ignoresSafeArea()
-//            Text("Экран настроек")
-//                .font(.custom("SFPro-Bold", size: 17))
-            ErrorView(imageName: "NoInternetError", text: "Нет интернета")
-//            ErrorView(imageName: "ServerError", text: "Ошибка сервера")
+        @Bindable var themeManager = themeManager
+        
+        NavigationStack(path: $navigationPath) {
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                VStack {
+                    VStack(spacing: 30) {
+                        HStack {
+                            Text("Темная тема")
+                                .font(.custom("SFPro-Regula", size: 17))
+                            Spacer()
+                            Toggle("", isOn: $themeManager.isDarkMode)
+                        }
+                        HStack {
+                            Text("Пользовательское соглашение")
+                                .font(.custom("SFPro-Regula", size: 17))
+                            Spacer()
+                            Button(action: {
+                                navigationPath.append("UserAgreement")
+                            }) {
+                                Image("NextButton")
+                            }
+                        }
+                    }
+                    Spacer()
+                    VStack(spacing: 10) {
+                        Text("Приложение использует API «Яндекс.Расписания»")
+                        Text("Версия 1.0 (beta)")
+                    }
+                    .font(.custom("SFPro-Regula", size: 12))
+                    .padding(.bottom, 60)
+                }
+                .padding()
+                .navigationDestination(for: String.self) { route in
+                    if route == "UserAgreement" {
+                        ZStack {
+                            Color.background
+                                .ignoresSafeArea()
+                            VStack{
+                                CustomNavigation(title: "Информация о компании") {
+                                    navigationPath.removeLast()
+                                }
+                                .background(Color.background)
+                                UserAgreementView(hideTabBar: $hideTabBar)
+                            }
+                                .navigationBarHidden(true)
+                        }
+                    }
+                }
+            }
+            .background(Color.background)
+        }
+        .onChange(of: navigationPath) { oldValue, newValue in
+            withAnimation(.easeInOut(duration: 0.25)) {
+                hideTabBar = !newValue.isEmpty
+            }
         }
     }
 }
